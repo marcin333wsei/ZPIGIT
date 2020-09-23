0dd6d5cdad002b73a3350c8474c4570574490403
damprog <damprog.one@gmail.com>
2020-09-23 21:51:13 +0200

    Kolejny commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main() {
    cout << "Hello, World!" << endl;
    cout << "Today is good weather." << endl;
    cout << "It's my example of code for git exercise. " << endl;
+   cout << "Commit after slack configuration " << endl;
 
 
     return 0;
