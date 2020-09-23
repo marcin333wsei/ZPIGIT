a0dc33b10660c15c2dfbce01b656e4d8d6ce8d9b
damprog <damprog.one@gmail.com>
2020-09-23 21:54:35 +0200

    commit dla slacka na galaz master

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    cout << "Today is good weather." << endl;
+    cout << "It's my example of code for git exercise. " << endl;
+    cout << "Commit after slack configuration! " << endl;
+
+
     return 0;
 }
