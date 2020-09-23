fdf63687c4bd24ca925865333c0e542cf980f2df
damprog <damprog.one@gmail.com>
2020-09-23 21:27:44 +0200

    Kolejny commit

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@ using namespace std;
 int main() {
    cout << "Hello, World!" << endl;
    cout << "Today is good weather." << endl;
+   cout << "It's my example of code for git exercise. " << endl;
+
 
     return 0;
 }
